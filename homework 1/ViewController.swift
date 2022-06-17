0d764cfd72bdf7d1001160a3ace46ac55aaa221a
Serov Dmitriy <serov>
2022-06-17 17:40:21 +0500

    Initial Commit

---
+++ homework 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework 1
+//
+//  Created by Dima on 17.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
